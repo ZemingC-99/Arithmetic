a1d7d7ceee5f42d33a445c05766f53ed3a0dd351
Zeming Chen <zceric@bu.edu>
2023-09-18 01:16:31 -0400

    Initial commit, basically finished calculator

---
+++ app/src/main/java/com/cs501/myapplication/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.cs501.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultText: TextView
+    private lateinit var operationSpinner: Spinner
+    private lateinit var firstNum: EditText
+    private lateinit var secondNum: EditText
+    private lateinit var calculateButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultText = findViewById(R.id.resultText)
+        operationSpinner = findViewById(R.id.operationSpinner)
+        firstNum = findViewById(R.id.firstNum)
+        secondNum = findViewById(R.id.secondNum)
+        calculateButton = findViewById(R.id.calculateButton)
+        calculateButton.setOnClickListener {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        val num1 = firstNum.text.toString().toDoubleOrNull() ?: return
+        val num2 = secondNum.text.toString().toDoubleOrNull() ?: return
+
+        val result = when (operationSpinner.selectedItem.toString()) {
+            "Add" -> num1 + num2
+            "Subtract" -> num1 - num2
+            "Multiply" -> num1 * num2
+            "Divide" -> if (num2 != 0.0) num1 / num2 else Double.NaN
+            else -> 0.0
+        }
+        resultText.text = result.toString()
+    }
+}
